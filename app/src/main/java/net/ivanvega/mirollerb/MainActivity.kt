01910b698bc6ac5ad21c9df2304e45018e1e93e2
JessicaG <s18120174@alumnos.itsur.edu.mx>
2022-09-07 22:40:22 -0500

    RollerDice

---
+++ app/src/main/java/net/ivanvega/mirollerb/MainActivity.kt
@@ -0,0 +1,44 @@
+package net.ivanvega.mirollerb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var etiqueta: TextView
+    lateinit var boton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_roller)
+
+        boton = findViewById(R.id.btnClick)
+        etiqueta = findViewById(R.id.lblRoller)
+        //forma larga de asignar listener
+        /*
+        boton.setOnClickListener(View.OnClickListener {
+            Toast.makeText(applicationContext,
+                "Boton presionado", Toast.LENGTH_LONG ).show()
+        })*/
+
+        boton.setOnClickListener {
+            Toast.makeText(applicationContext,
+                "Boton presionado", Toast.LENGTH_LONG ).show()
+
+            val aleatorio = diceRoller()
+            etiqueta.text = "Alatorio ${aleatorio}"
+
+        }
+    }
+
+    fun diceRoller(par: Int = 0): Int{
+        return (1..6).random()
+    }
+
+    private fun getRandomDiceImage() : Int {
+
+    }
+}
